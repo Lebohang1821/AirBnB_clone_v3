82d95aa5da56ddc5d59e2d395d12d2d6774a53e4
Lebohang1821 <millboylebohang@gmail.com>
2024-02-11 12:16:41 +0000

    task-2

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Flask App that integrates with AirBnB static HTML Template
+It integrates with AirBnB static HTML Template
 """
 from flask import Flask, render_template, url_for
 from models import storage
@@ -17,7 +17,7 @@ host = '0.0.0.0'
 @app.teardown_appcontext
 def teardown_db(exception):
     """
-    after each request, this method calls .close() (i.e. .remove()) on
+    after each request, this method calls
     the current SQLAlchemy Session
     """
     storage.close()
@@ -26,7 +26,7 @@ def teardown_db(exception):
 @app.route('/2-hbnb')
 def hbnb_filters(the_id=None):
     """
-    handles request to custom template with states, cities & amentities
+    It handles request to custom template with states, cities & amentities
     """
     state_objs = storage.all('State').values()
     states = dict([state.name, state] for state in state_objs)
