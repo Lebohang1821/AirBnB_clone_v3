bfcfb8bb747976e24829910ec272c4fe3829da8e
Lebohang1821 <millboylebohang@gmail.com>
2024-02-11 12:27:47 +0000

    task-2

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', function () {
-  // task 3
+  // For task 3
   $.ajax('http://0.0.0.0:5001/api/v1/status').done(function (data) {
     if (data.status === 'OK') {
       $('#api_status').addClass('available');
